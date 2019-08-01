570a0f21d022ab9d217262f1a32f79897a2210a5
Cem Turesoy <cemturesoy@codeventure.com.tr>
2019-08-01 15:25:12 +0300

    add: some delete stuff

--- index.js
+++ index.js
@@ -90,7 +90,9 @@ const typeDefs = `
     createUser(data: CreateUserInput): User!
     deleteUser(id: ID!): User!
     createPost(data: CreatePostInput): Post!
+    deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   input CreateUserInput {
@@ -223,6 +225,15 @@ const resolvers = {
       posts.push(post);
       return post;
     },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+      if (postIndex === -1) throw new Error("Post does not exist");
+
+      const deletedPosts = posts.splice(postIndex, 1);
+      comments = comments.filter(comment => comment.post !== args.id);
+
+      return deletedPosts[0];
+    },
     createComment(parent, args, ctx, info) {
       const userExist = users.some(user => user.id === args.data.author);
       if (!userExist) throw new Error("User does not exist.");
@@ -239,6 +250,15 @@ const resolvers = {
       };
       comments.push(comment);
       return comment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(
+        comment => comment.id === args.id
+      );
+      if (commentIndex === -1) throw new Error("Comment does not exist");
+
+      const deletedComments = comments.splice(commentIndex, 1);
+      return deletedComments[0];
     }
   },
   Post: {
